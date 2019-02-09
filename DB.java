dc3d4e424bf11958906bfa63d85c92204346f2ff
welchcode <42048962+welchcode@users.noreply.github.com>
2019-02-09 12:24:08 -0600

    Update DB.java
    adding comments later

--- DB.java
+++ DB.java
@@ -5,17 +5,20 @@ import java.sql.*;
  * 
  * */
 public class DB {
+	//declaration of private vars
+	private static String url = "jdbc:postgresql://localhost:5432/store_db";		// URL used to connect 
+	private static Connection conn;
+	private static Statement stmt;
+	
 	//Main Database Method
 	public static void connect() throws SQLException {
-		String url = "jdbc:postgresql://localhost:5432/store_db";			// URL used to connect 
-		Connection conn = DriverManager.getConnection(url,null,null);		//getConnection used for connecting program to local DB
+		
+		conn = DriverManager.getConnection(url,null,null);		//getConnection used for connecting program to local DB
 		if(conn != null)
 			System.out.println("Database connected successfully");		//if the connection is successful, print this
 	}
 	//This method, in the future will allow updates to happen to the database -- Its current state is for testing purposes
-	public static void updateCompany() throws SQLException {
-		String url = "jdbc:postgresql://localhost:5432/store_db";
-		Statement stmt = null;
+	public static void addItem(int id, String name, double price, int quantity) throws SQLException {		
 		Connection conn = DriverManager.getConnection(url,null,null);		//reconnecting the database to the program-- this will be rewritten as its own method later
 		if(conn != null)
 			System.out.println("Database connected successfully");
@@ -23,11 +26,18 @@ public class DB {
 		
 		stmt = conn.createStatement();
 		String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " + // current test statement
-					"VALUES (3,'Paul',32,'California',20000.00);";
+					"VALUES "+"("+id+","+"'"+name+"',"+price+","+quantity+");";
 		stmt.executeUpdate(sql);
 		System.out.println("Update successful");
 		stmt.close();
 		conn.commit();
 		conn.close();
+	}
+	public static void addEmployee(String id, String firstName, String lastName,  String age, String streetAddress, String state, String salary) throws SQLException {
+		stmt = conn.createStatement();
+		String sql = "INSERT INTO EMPLOYEE (ID,FIRST_NAME,LAST_NAME,AGE,ADDRESS,STATE,SALARY) " + // current test statement
+				"VALUES "+"('"+id+"',"+"'"+firstName+"','"+lastName+"','"+age+"','"+streetAddress+"','"+state+"','"+salary+"')";		
+		stmt.executeUpdate(sql);
+		System.out.println("Update successful");
 	}
 }
